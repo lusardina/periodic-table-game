fc3e7ad043b7a2b70da0c18793213d76b725bb4d
lusardina <lucia.sardina@gmail.com>
2023-10-26 13:10:15 +1100

    Add bit of code

--- main.py
+++ main.py
@@ -3,17 +3,38 @@ import pygame
 # colours
 BLUE = (0,128,255)
 PURPLE = (170,128,255)
-RED = (204,34,0)
+RED = (194,59,34)
 YELLOW = (255,234,128)
+ORANGE = (255,179,71)
+CREAM = (255,247,230)
+VIOLET = (203,153,201)
+PINK = (244,154,194)
 
 # screen
 screen = pygame.display.set_mode((1100, 713))
 pygame.display.set_caption("Periodic Table Game - Lucia. S") # screen caption
-screen.fill(YELLOW) 
-background = pygame.image.load('periodic-table2.jpg') 
-screen.blit(background, (0, 0)) 
+screen.fill(PURPLE) 
+#background = pygame.image.load('periodic-table2.jpg') # background picture of a periodic table
+#screen.blit(background, (0, 0)) 
 pygame.display.flip() # update full display to screen
-  
+
+'''
+# metal colours shown on table
+type_colours = {
+    "Alkali Metal": "", 
+    "Alkaline Earth Metal": "",
+    "Transition Metal": "",
+    "Nonmetal": "",
+    "Halogen": "",
+    "Noble Gas": "",
+    "Lanthanide": "",
+    "Actinide": "", 
+    "Metalloid": "", 
+    "Reactive Nonmetals": "", 
+    "Unknown Properties": "", 
+    }  
+'''
+
 
 # keep game loop running
 running = True
