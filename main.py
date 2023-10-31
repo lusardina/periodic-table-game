f3a8c6d8ad748fb9e1ed295f577bdc76204e2270
Your Name <you@example.com>
2023-10-31 15:04:16 +1100

    start object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import csv
 
 # colours
 BLUE = (0,128,255)
@@ -10,12 +11,33 @@ CREAM = (255,247,230)
 VIOLET = (203,153,201)
 PINK = (244,154,194)
 
+'''
+def element_file(file="data_elements.csv"):
+    #prints csv
+    with open('eggs.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            print(', '.join(row))
+'''
+
+class elements:
+    def __init__(self, name, atomic_num, symbol, weight, col, row):
+        self.name = name
+        self.atomic_number = atomic_num
+        self.symbol = symbol
+        self.column = col
+        self.row = row
+    def draw_element(self):
+        pass
+
 # screen
 screen = pygame.display.set_mode((1100, 713))
 pygame.display.set_caption("Periodic Table Game - Lucia. S") # screen caption
-screen.fill(PURPLE) 
-#background = pygame.image.load('periodic-table2.jpg') # background picture of a periodic table
-#screen.blit(background, (0, 0)) 
+screen.fill(CREAM) 
+'''
+background = pygame.image.load('periodic-table2.jpg') # background picture of a periodic table
+screen.blit(background, (0, 0)) 
+'''
 pygame.display.flip() # update full display to screen
 
 '''
