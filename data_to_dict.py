f13c5a4479fe9f76622f1a80c89796115f66e31f
lusardina <lucia.sardina@gmail.com>
2023-11-17 05:49:14 +1100

    Add listing of properties #2

--- data_to_dict.py
+++ data_to_dict.py
@@ -4,16 +4,21 @@ elements_data = {}
 
 with open("data_elements.csv") as file_data_elements:
     for line in csv.DictReader(file_data_elements):
-        elements_data[line["Element"]] = dict(line) # adds each row of CSV file into the dictionary "elements_data"
+        elements_data[line["Element"]] = line # adds each row of CSV file into the dictionary "elements_data"
 
-print(elements_data) # it works!!!
+# print(elements_data) # it works!!!
 
-"""
+
 element = input("What element do you want to access? ").title()
 while True:
-    what_data = input(f"What would you like to know about the element {element}? (if nothing type 'nothing'): ")
+    what_data = input(f"What would you like to know about the element {element}? ('nothing' to finish; 'list' to print possible properties ): ")
     if what_data.lower() == "nothing":
         break
-    answer = elements_data[element][what_data]
-    print(f"The {what_data} of {element} is {answer}!")
-"""
+    elif what_data.lower() == "list":
+        print("here is a list of the properties you can choose from:")
+        for key in elements_data[element]:
+            print(f"- {key}")
+    else:       
+        answer = elements_data[element][what_data]
+        print(f"The {what_data} of {element} is {answer}!")
+
