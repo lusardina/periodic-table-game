9ae4719af8d5d7eb7471093beef9f08b625fe6e2
lusardina <lucia.sardina@gmail.com>
2023-12-08 08:28:41 +1100

    Draw periodic table of elements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import csv
 from data_to_dict import elements_data
+pygame.font.init()
 
 # colours
 BLUE = (0,128,255)
@@ -15,6 +16,16 @@ GREY = (132,132,130)
 EMERALD = (49,145,119)
 DARK_PINK = (171,39,79)
 TEAL = (0,128,128)
+WIDTH = 800
+HEIGHT = 600
+
+# screen
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Periodic Table Game - Lucia. S") # screen caption
+SCREEN.fill(CREAM) 
+
+# other constants
+FONT = pygame.font.Font('freesansbold.ttf', 16)
 
 '''
 def element_file(file="data_elements.csv"):
@@ -34,9 +45,24 @@ class elements:
         self.column = element_data["Group"]
         self.row = element_data["Period"]
         self.type = element_data["Type"]
+        self.width = WIDTH / 18
+        self.height = HEIGHT / 9
+        self.drawable = True
+        if self.column == "":
+            self.drawable = False
+            return
+        if int(self.column) < 3:
+            self.x_pos = (int(self.column) - 1) * self.width
+        else:
+            self.x_pos = (int(self.column) - 2) * self.width
+        self.y_pos = (int(self.row) - 2) * self.height
+
     def draw_element(self):
         # element colours shown on table
-        print(self.symbol)
+        if not self.drawable:
+            return
+        
+        # print(self.symbol)
         type_colours = {
             "Alkali Metal": RED, 
             "Alkaline Earth Metal": ORANGE,
@@ -50,25 +76,34 @@ class elements:
             "Reactive Nonmetals": EMERALD, 
             "Unknown Properties": GREY, 
             }  
-        pygame.draw.rect(screen, type_colours[self.type], pygame.Rect(30, 30, 60, 60))
+        # print(self.drawable)
+        if self.type in type_colours:
+            pygame.draw.rect(SCREEN, type_colours[self.type], pygame.Rect(self.x_pos, self.y_pos, self.width, self.height))
+        else:
+            pygame.draw.rect(SCREEN, GREY, pygame.Rect(self.x_pos, self.y_pos, self.width, self.height))
+        # print(self.x_pos, self.y_pos, self.width, self.height)
+        SCREEN.blit(FONT.render(self.atomic_number, True, 'black'), (self.x_pos + 5, self.y_pos + 5))
+        SCREEN.blit(FONT.render(self.symbol, True, 'black'), (self.x_pos + 5, self.y_pos + 20))
         pygame.display.flip()
-        pass
-
-# screen
-screen = pygame.display.set_mode((1100, 713))
-pygame.display.set_caption("Periodic Table Game - Lucia. S") # screen caption
-screen.fill(CREAM) 
+        return
+        
 
 
+
+"""
 background = pygame.image.load('periodic-table2.jpg') # background picture of a periodic table
 screen.blit(background, (0, 0)) 
+"""
 
 pygame.display.flip() # update full display to screen
 
 
-hydrogen = elements("Hydrogen", elements_data)
-hydrogen.draw_element()
+#hydrogen = elements("Carbon", elements_data)
+#hydrogen.draw_element()
 
+for e in elements_data:
+    element = elements(e, elements_data)
+    element.draw_element()
 # keep game loop running
 running = True
 # game loop 
